a405a6eaaea29f00e57c44413a2b6f26c8a3faec
AdamEXu <51470750+AdamEXu@users.noreply.github.com>
2022-07-30 13:10:59 -0400

    Add files via upload

---
+++ sashax.py
@@ -0,0 +1,66 @@
+import sys
+import os
+from os import system as ex
+
+userpath = os.path.expanduser("~")
+print(userpath)
+
+print("SasHax is loading...")
+print("Note that SasHax may need administrative privileges to run. Please enter your password to continue:")
+os.system("sudo echo Authenticated...")
+print("SasHax is ready to use!")
+query = input("Press enter to continue...")
+while query != "Q":
+    print("Please enter a command:")
+    print("D - Duplicate a save")
+    print("C - Modify a save")
+    print("Q - Quit")
+    query = input("Command: ")
+    if query == "D":
+        print("WARNING! When duplicating a save, it replaces the original save with the new save. It may also remove backups. Please only duplicate to saves you know you don't need anymore.")
+        print("Please enter the number of the save you wish to duplicate:")
+        savefrom = input("Save: ")
+        if savefrom == 1:
+            savefrom = ""
+        print("Please enter the number of the save you wish to replace:")
+        saveto = input("Save: ")
+        if saveto == 1:
+            saveto = ""
+        print("Duplicating save...")
+        ex(f"cp -r {userpath}/Library/Containers/com.rac7.SneakySasquatchMac/Data/Library/Application Support/com.rac7.SneakySasquatchMac/default{savefrom} {userpath}/Library/Containers/com.rac7.SneakySasquatchMac/Data/Library/Application Support/com.rac7.SneakySasquatchMac/default{saveto}")
+        print("Save duplicated!")
+    if query == "C":
+        print("Currently, this feature can only modify coins. Other modifications are coming soon.")
+        print("Please enter the number of the save you wish to modify:")
+        save = input("Save: ")
+        if save == 1:
+            save = ""
+        print("Please enter the amount of coins you wish to have:")
+        coins = input("Coins: ")
+        print("Modifying save...")
+        with open(f"{userpath}/Library/Containers/com.rac7.SneakySasquatchMac/Data/Library/Application Support/com.rac7.SneakySasquatchMac/default{save}/sasquatch.stuff", "r") as f:
+            line = f.readline()
+        digits = 0
+        index = line.find("coins")+7
+        hitnext = False
+        while not hitnext:
+            try:
+                int(line[index+digits])
+                digits += 1
+            except:
+                hitnext = True
+        print(line[index:index+digits])
+        beforecoins = int(line[index:index+digits])
+        ls = list(line)
+        ls[index:index+digits+1] = str(coins)
+        line = "".join(ls)
+        with open(f"{userpath}/Library/Containers/com.rac7.SneakySasquatchMac/Data/Library/Application Support/com.rac7.SneakySasquatchMac/default{save}/sasquatch.stuff", "w") as f:
+            f.write(line)
+        print("Save successfully modified!")
+    if query == "Q":
+        print("Goodbye.")
+        os.system("cd")
+        sys.exit()
+    else:
+        print("Unknown command.")
+
