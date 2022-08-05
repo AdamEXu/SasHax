9077f0de31da04eba457ba49b309576920cdf4d8
AdamEXu <51470750+AdamEXu@users.noreply.github.com>
2022-08-04 22:31:46 -0400

    Update sashax.py
    Added lumber modifications!

--- sashax.py
+++ sashax.py
@@ -30,33 +30,66 @@ while query != "Q":
         ex(f"cp -r {userpath}/Library/Containers/com.rac7.SneakySasquatchMac/Data/Library/Application Support/com.rac7.SneakySasquatchMac/default{savefrom} {userpath}/Library/Containers/com.rac7.SneakySasquatchMac/Data/Library/Application Support/com.rac7.SneakySasquatchMac/default{saveto}")
         print("Save duplicated!")
     if query == "C":
-        print("Currently, this feature can only modify coins. Other modifications are coming soon.")
-        print("Please enter the number of the save you wish to modify:")
-        save = input("Save: ")
-        if save == 1:
-            save = ""
-        print("Please enter the amount of coins you wish to have:")
-        coins = input("Coins: ")
-        print("Modifying save...")
-        with open(f"{userpath}/Library/Containers/com.rac7.SneakySasquatchMac/Data/Library/Application Support/com.rac7.SneakySasquatchMac/default{save}/sasquatch.stuff", "r") as f:
-            line = f.readline()
-        digits = 0
-        index = line.find("coins")+7
-        hitnext = False
-        while not hitnext:
-            try:
-                int(line[index+digits])
-                digits += 1
-            except:
-                hitnext = True
-        print(line[index:index+digits])
-        beforecoins = int(line[index:index+digits])
-        ls = list(line)
-        ls[index:index+digits+1] = str(coins)
-        line = "".join(ls)
-        with open(f"{userpath}/Library/Containers/com.rac7.SneakySasquatchMac/Data/Library/Application Support/com.rac7.SneakySasquatchMac/default{save}/sasquatch.stuff", "w") as f:
-            f.write(line)
-        print("Save successfully modified!")
+        print("Please enter a modification:")
+        print("C - Coins")
+        print("L - Lumber")
+        # print("I - Inventory")
+        # print("D - Disguise")
+        print("E - Exit")
+        modification = input("Modification: ")
+        if modification == "C":
+            print("Please enter the number of the save you wish to modify:")
+            save = input("Save: ")
+            if save == 1:
+                save = ""
+            print("Please enter the amount of coins you wish to have:")
+            coins = input("Coins: ")
+            print("Modifying save...")
+            with open(f"{userpath}/Library/Containers/com.rac7.SneakySasquatchMac/Data/Library/Application Support/com.rac7.SneakySasquatchMac/default{save}/sasquatch.stuff", "r") as f:
+                line = f.readline()
+            digits = 0
+            index = line.find("coins")+7
+            hitnext = False
+            while not hitnext:
+                try:
+                    int(line[index+digits])
+                    digits += 1
+                except:
+                    hitnext = True
+            # print(line[index:index+digits])
+            beforecoins = int(line[index:index+digits])
+            ls = list(line)
+            ls[index:index+digits+1] = str(coins)
+            line = "".join(ls)
+            with open(f"{userpath}/Library/Containers/com.rac7.SneakySasquatchMac/Data/Library/Application Support/com.rac7.SneakySasquatchMac/default{save}/sasquatch.stuff", "w") as f:
+                f.write(line)
+            print("Save successfully modified!")
+        elif modification == "L":
+            print("Please enter the number of the save you wish to modify:")
+            save = input("Save: ")
+            if save == 1:
+                save = ""
+            print("Please enter the amount of lumber you wish to have:")
+            lumber = input("Lumber: ")
+            print("Modifying save...")
+            with open(f"{userpath}/Library/Containers/com.rac7.SneakySasquatchMac/Data/Library/Application Support/com.rac7.SneakySasquatchMac/default{save}/sasquatch.stuff", "r") as f:
+                line = f.readline()
+            digits = 0
+            index = line.find("lumber")+8
+            hitnext = False
+            while not hitnext:
+                try:
+                    int(line[index+digits])
+                    digits += 1
+                except:
+                    hitnext = True
+            beforelumber = int(line[index:index+digits])
+            ls = list(line)
+            ls[index:index+digits+1] = str(lumber)
+            line = "".join(ls)
+            with open(f"{userpath}/Library/Containers/com.rac7.SneakySasquatchMac/Data/Library/Application Support/com.rac7.SneakySasquatchMac/default{save}/sasquatch.stuff", "w") as f:
+                f.write(line)
+            print("Save successfully modified!")
     if query == "Q":
         print("Goodbye.")
         os.system("cd")
